3474fb845f770955f2babf8df17d96b89f07026e
1753025 <34498550+AnTruongQuoc@users.noreply.github.com>
2021-08-04 00:54:48 +0700

    update navigationbar and sth

--- app/src/main/java/com/quintus/labs/datingapp/Utils/TopNavigationViewHelper.java
+++ app/src/main/java/com/quintus/labs/datingapp/Utils/TopNavigationViewHelper.java
@@ -1,24 +1,19 @@
 package com.quintus.labs.datingapp.Utils;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import androidx.annotation.NonNull;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import android.util.Log;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 import com.quintus.labs.datingapp.Main.MainActivity;
 import com.quintus.labs.datingapp.Matched.Matched_Activity;
 import com.quintus.labs.datingapp.Profile.Profile_Activity;
 import com.quintus.labs.datingapp.R;
-
-/**
- * DatingApp
- * https://github.com/quintuslabs/DatingApp
- * Created on 25-sept-2018.
- * Created by : Santosh Kumar Dash:- http://santoshdash.epizy.com
- */
 
 public class TopNavigationViewHelper {
 
@@ -32,27 +27,37 @@ public class TopNavigationViewHelper {
 
     public static void enableNavigation(final Context context, BottomNavigationViewEx view) {
         view.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @SuppressLint("NonConstantResourceId")
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch (item.getItemId()) {
                     case R.id.ic_profile:
                         Intent intent2 = new Intent(context, Profile_Activity.class);
                         context.startActivity(intent2);
+                        item.setChecked(true);
                         break;
 
                     case R.id.ic_main:
                         Intent intent1 = new Intent(context, MainActivity.class);
                         context.startActivity(intent1);
+                        item.setChecked(true);
                         break;
 
                     case R.id.ic_matched:
                         Intent intent3 = new Intent(context, Matched_Activity.class);
                         context.startActivity(intent3);
-
+                        item.setChecked(true);
                         break;
+                    case R.id.ic_like:
+                    case R.id.ic_search:
+                        //do nothing
+                        item.setChecked(true);
+                        break;
+
+
                 }
 
-                return false;
+                return true;
             }
         });
     }
